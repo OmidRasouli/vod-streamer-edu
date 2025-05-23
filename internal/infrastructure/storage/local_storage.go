6b262647f0452058cf387b5db916fb53bb377463
OMiD <o.rasouli92@gmail.com>
2025-05-23 20:42:39 +0330

    Add local storage functios to store files to the local storage

---
+++ internal/infrastructure/storage/local_storage.go
@@ -0,0 +1,59 @@
+package storage
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/OmidRasouli/vod-streamer-edu/internal/domain/port"
+)
+
+type LocalStorage struct {
+	BasePath string
+}
+
+func NewLocalStorage(basePath string) port.VideoStorage {
+	return &LocalStorage{
+		BasePath: basePath,
+	}
+}
+
+func (s *LocalStorage) Save(reader io.Reader, filename string) error {
+	fullPath := filepath.Join(s.BasePath, filename)
+
+	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
+		return fmt.Errorf("failed to create directories: %w", err)
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, reader); err != nil {
+		return fmt.Errorf("failed to save video data: %w", err)
+	}
+
+	return nil
+}
+
+func (s *LocalStorage) Open(filename string) (io.ReadCloser, error) {
+	fullPath := filepath.Join(s.BasePath, filename)
+	file, err := os.Open(fullPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open video file: %w", err)
+	}
+	return file, nil
+}
+
+func (s *LocalStorage) GetPath(filename string) (string, error) {
+	fullPath := filepath.Join(s.BasePath, filename)
+
+	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
+		return "", fmt.Errorf("file does not exist: %s", fullPath)
+	}
+
+	return fullPath, nil
+}
