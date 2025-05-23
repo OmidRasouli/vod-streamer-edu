941e211d33b98e5b81ccfc378b481c944600559c
OMiD <o.rasouli92@gmail.com>
2025-05-23 18:24:35 +0330

    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"strconv"
+
+	"github.com/OmidRasouli/vod-streamer-edu/configs"
+	"github.com/OmidRasouli/vod-streamer-edu/internal/controller/http"
+)
 
 func main() {
-	fmt.Println("Vod Streamer Server Started")
+	cfg := configs.MustLoad("configs/config.yaml")
+
+	runRouter(cfg)
+}
+
+func runRouter(cfg *configs.Config) {
+	// Set up HTTP server
+	router := http.NewRouter()
+	port := cfg.GetServerConfig().Port
+
+	if err := router.Run(":" + strconv.Itoa(port)); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
+	log.Printf("Server is running on port %d", port)
 }
