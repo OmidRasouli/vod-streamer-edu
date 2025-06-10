36c5d7420ddaa44abb9a1cbeaffaf0eadff6a19e
OMiD <o.rasouli92@gmail.com>
2025-06-11 01:35:25 +0330

    Replace path type with string

--- internal/domain/port/storage.go
+++ internal/domain/port/storage.go
@@ -1,9 +1,13 @@
 package port
 
-import "io"
+import (
+	"io"
+
+	"github.com/OmidRasouli/vod-streamer-edu/internal/entity"
+)
 
 type VideoStorage interface {
-	Save(reader io.Reader, path ...string) error // Save video data to storage
-	Open(filename string) (io.ReadCloser, error) // Open video file for reading
-	GetPath(path ...string) (string, error)      // Get absolute path of stored video file
+	Save(reader io.Reader, path ...string) (entity.Path, error) // Save video data to storage
+	Open(path ...string) (io.ReadCloser, error)                 // Open video file for reading
+	GetPath(path ...string) (entity.Path, error)                // Get absolute path of stored video file
 }
--- internal/infrastructure/storage/local_storage.go
+++ internal/infrastructure/storage/local_storage.go
@@ -7,40 +7,41 @@ import (
 	"path/filepath"
 
 	"github.com/OmidRasouli/vod-streamer-edu/internal/domain/port"
+	"github.com/OmidRasouli/vod-streamer-edu/internal/entity"
 )
 
 type LocalStorage struct {
-	BasePath string
+	BasePath entity.Path
 }
 
 func NewLocalStorage(basePath string) port.VideoStorage {
 	return &LocalStorage{
-		BasePath: basePath,
+		BasePath: entity.NewPath(basePath),
 	}
 }
 
-func (s *LocalStorage) Save(reader io.Reader, path ...string) error {
-	fullPath := filepath.Join(append([]string{s.BasePath}, path...)...)
+func (s *LocalStorage) Save(reader io.Reader, path ...string) (entity.Path, error) {
+	fullPath := s.BasePath.Join(path...).String()
 
 	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
-		return fmt.Errorf("failed to create directories: %w", err)
+		return entity.Path{}, fmt.Errorf("failed to create directories: %w", err)
 	}
 
 	file, err := os.Create(fullPath)
 	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
+		return entity.Path{}, fmt.Errorf("failed to create file: %w", err)
 	}
 	defer file.Close()
 
 	if _, err := io.Copy(file, reader); err != nil {
-		return fmt.Errorf("failed to save video data: %w", err)
+		return entity.Path{}, fmt.Errorf("failed to save video data: %w", err)
 	}
 
-	return nil
+	return entity.StringPathToPath(fullPath), nil
 }
 
-func (s *LocalStorage) Open(filename string) (io.ReadCloser, error) {
-	fullPath := filepath.Join(s.BasePath, filename)
+func (s *LocalStorage) Open(path ...string) (io.ReadCloser, error) {
+	fullPath := s.BasePath.Join(path...).String()
 	file, err := os.Open(fullPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open video file: %w", err)
@@ -48,12 +49,12 @@ func (s *LocalStorage) Open(filename string) (io.ReadCloser, error) {
 	return file, nil
 }
 
-func (s *LocalStorage) GetPath(path ...string) (string, error) {
-	fullPath := filepath.Join(append([]string{s.BasePath}, path...)...)
+func (s *LocalStorage) GetPath(path ...string) (entity.Path, error) {
+	fullPath := s.BasePath.Join(path...).String()
 
 	if _, err := os.Stat(fullPath); os.IsNotExist(err) {
-		return "", fmt.Errorf("file does not exist: %s", fullPath)
+		return entity.Path{}, fmt.Errorf("file does not exist: %s", fullPath)
 	}
 
-	return fullPath, nil
+	return entity.StringPathToPath(fullPath), nil
 }
