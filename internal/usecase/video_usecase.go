b9e6284659dfe515a8cb1d9c65c9856d9a373744
OMiD <o.rasouli92@gmail.com>
2025-06-11 01:40:58 +0330

    Refactor VideoUsecase to include ffmpeg service and enhance ProcessAndSave method

--- internal/usecase/video_usecase.go
+++ internal/usecase/video_usecase.go
@@ -1,6 +1,7 @@
 package usecase
 
 import (
+	"fmt"
 	"io"
 
 	"github.com/OmidRasouli/vod-streamer-edu/internal/domain/port"
@@ -9,26 +10,53 @@ import (
 
 type VideoUsecase struct {
 	storage port.VideoStorage
+	ffmpeg  port.FFmpeg
 }
 
-func NewVideoUsecase(storage port.VideoStorage) *VideoUsecase {
-	return &VideoUsecase{storage: storage}
+func NewVideoUsecase(storage port.VideoStorage, ffmpeg port.FFmpeg) *VideoUsecase {
+	return &VideoUsecase{storage: storage, ffmpeg: ffmpeg}
 }
 
-// Save saves video data using the storage interface
-func (uc *VideoUsecase) Save(filename string, reader io.Reader) error {
+// ProcessAndSave saves video data using the storage interface
+func (uc *VideoUsecase) ProcessAndSave(filename string, reader io.Reader) error {
 	// Generate a new UUID for the filename
 	id := uuid.New().String()
 
-	return uc.storage.Save(reader, id, filename)
+	// Save the video file using the storage interface
+	// The video will be saved in the storage in a directory structure like: /videos/{id}/{filename}
+	savedDetails, err := uc.storage.Save(reader, id, filename)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Video saved with details:", savedDetails)
+
+	// Get video details using the ffmpeg service
+	// TODO: We will save it later in the database
+	videoDetails, err := uc.ffmpeg.GetVideoDetails(savedDetails)
+	if err != nil {
+		return err
+	}
+
+	// Check if the video is portrait
+	if videoDetails == nil {
+		return fmt.Errorf("no video details available") // No video details available, nothing to process
+	}
+	isPortrait := videoDetails.IsPortrait()
+
+	// Transcode the video using the ffmpeg service
+	if err := uc.ffmpeg.Transcode(savedDetails, isPortrait); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// Open returns a ReadCloser for the video file
-func (uc *VideoUsecase) Open(filename string) (io.ReadCloser, error) {
-	return uc.storage.Open(filename)
+func (uc *VideoUsecase) GetVideoSegment(fileID string, quality string, file string) string {
+	videoPath, _ := uc.storage.GetPath(fileID, "normal_hls", quality, file)
+	return videoPath.String()
 }
 
-// GetPath returns the absolute path of the video file
-func (uc *VideoUsecase) GetPath(filename string) (string, error) {
-	return uc.storage.GetPath(filename)
+func (uc *VideoUsecase) GetVideoMaster(fileID string) string {
+	playlistPath, _ := uc.storage.GetPath(fileID, "normal_hls", "master.m3u8")
+	return playlistPath.String()
 }
